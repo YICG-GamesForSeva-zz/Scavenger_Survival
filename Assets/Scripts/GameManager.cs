8bb8297a4aa9c20c9670044d18bf574925ce2903
Pranav Krishnamurthy <pkrish19@outlook.com>
2017-09-11 20:21:56 -0400

    Adding BoardManager, GameManager
    Board Manager is complete with all of the enemy, food, wall, outerwall tiles
    
    Game Manager is complete with determining how all of the prefabs are placed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    private int level = 3; // Testing out the enemies
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject); 
+        }
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame(); 
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
